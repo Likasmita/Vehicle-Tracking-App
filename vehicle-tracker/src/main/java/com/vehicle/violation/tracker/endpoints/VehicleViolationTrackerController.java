81590847839e36dc5be4c8df4464730550a881e8
Likasmita <daleilikasmita11@gmail.com>
2021-04-13 19:39:46 +0530

    Vehicle tracker eureka client added

--- vehicle-tracker/src/main/java/com/vehicle/violation/tracker/endpoints/VehicleViolationTrackerController.java
+++ vehicle-tracker/src/main/java/com/vehicle/violation/tracker/endpoints/VehicleViolationTrackerController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,6 +25,7 @@ import com.vehicle.violation.tracker.services.VehicleViolationService;
 
 
 @RestController
+@EnableEurekaClient
 public class VehicleViolationTrackerController {
 	
 
